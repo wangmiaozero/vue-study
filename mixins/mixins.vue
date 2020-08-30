d953dc9d257ae51438da66fa4cd5f3b08270278f
wangmiaozero <1061214467@qq.com>
2020-08-30 11:08:06 +0800

    mixins

---
+++ mixins/mixins.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    home
+</div>
+</template>
+<script>
+import {mixinsTest} from './util/mixins.js'
+export default {
+  name: "Home",
+  data () {
+    return {
+    };
+  },
+  created(){
+      console.log("home");
+      
+  },
+  //mixins的created会先被调用,然后再执行组件的created
+  mixins:[mixinsTest]
+}
+</script>
---
+++ mixins/util/mixins.js
@@ -0,0 +1,10 @@
+export const mixinsTest = {
+   created() {
+       this.hello();
+   },
+   methods: {
+     hello(){
+         console.log('hollo 方法')
+     }  
+   }, 
+}
